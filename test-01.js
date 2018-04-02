d77ac335d2df0d575ed5114f4765930ed99f4f08
Maxim <mzhukov31415dev@gmail.com>
2018-04-02 21:19:23 +0300

    Initial commit

---
+++ test-01.js
@@ -0,0 +1,52 @@
+   const doIt = (a, topNum = 5, ceiling = 100) => {
+     let topNumArr = [];
+     topNumArr.length = topNum;
+     topNumArr.fill({sum:0,combo:[]});
+
+     const fn = (n,srcArr,combo) => {
+       if (n==0) {
+         if (combo.length>0) {
+           let sum=0;
+           sum=combo.reduce((total,num)=>{return total+num});
+           if (sum<=ceiling) { /*If sum<ceiling:*/
+             if (sum>topNumArr[0].sum) { /*AND sum>(minTopNum) then store as proper (push to topNumArr, sort topNumArr by value and loose the lowest element)*/
+               const sall = {'sum':sum,'combo':combo};
+               topNumArr.push(sall);
+               topNumArr.sort((a,b)=>{return a.sum-b.sum});
+               topNumArr.splice(0,1);//Remove the first element
+             }
+           }
+         }
+         return;
+       }
+       for (let j=0;j<srcArr.length;++j) {
+         fn(n-1, srcArr.slice(j+1), combo.concat([srcArr[j]]));
+       }
+       return;
+     }
+
+     for (let m=1;m<a.length-1;m++) {
+       fn(m,a,[]);
+     }
+     return topNumArr;
+   }
+
+  function preDoIt() {
+    let arr_in;
+    if (document.getElementById('arrManual').value.length>0) {
+      arr_in = document.getElementById('arrManual').value.split(' ');
+      arr_in.forEach ((val,idx,arr)=>{arr[idx]=isNaN(parseInt(val))?0:parseInt(val)});
+      document.getElementById('inpArray').innerHTML = 'Resulting input array: ['+arr_in.toString()+']';
+    } else {
+      document.getElementById('inpArray').innerHTML = document.getElementById('arrManual').value.length;
+    }
+    const res = doIt(arr_in,5,100);
+    let strRes = '';
+    for (let i=0;i<res.length-1;i++) {
+      strRes += res[i].sum + ':' + res[i].combo.toString() + ']<br>[';
+    }
+    strRes += res[res.length-1].sum + ':' + res[res.length-1].combo.toString() + ']';
+    document.getElementById('firstFive').innerHTML = 'First five element combos with sum <=100:<br>['+strRes+'';
+    console.log('arr_in',arr_in);
+    console.log(res);
+  }
